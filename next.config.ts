ea0b7e4f665083f141d06f813a027f518e2d523c
ABHINAV <rajabhinav734@gmail.com>
2026-06-17 11:50:55 +0530

    chore: remove www-to-non-www domain redirect from next config

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/:path*',
-        has: [
-          {
-            type: 'host',
-            value: 'www.sanitatepharma.com',
-          },
-        ],
-        destination: 'https://sanitatepharma.com/:path*',
-        permanent: true,
-      },
-    ];
-  },
   images: {
     remotePatterns: [
       {
